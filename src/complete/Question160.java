19b0f5ab70edc8c8e414dd17f3b6dfb1f094efd0
Winton Gee <wintongee1@gmail.com>
2022-06-08 00:37:43 -0700

    Update

--- src/complete/Question160.java
+++ src/complete/Question160.java
@@ -23,4 +23,42 @@ public class Question160 {
         return null;
     }
 
+    // Second attempt to make it O(1) for memory and O(N) for time complexity
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+
+        int aSize = getSize(headA), bSize = getSize(headB);
+
+        // Case: ListNode headA is bigger
+        while (aSize > bSize) {
+            aSize--;
+            headA = headA.next;
+        }
+
+        // Case: ListNode headB is bigger
+        while (bSize > aSize) {
+            bSize--;
+            headB = headB.next;
+        }
+
+        // Setting the intersecting points
+        while (headA != headB) {
+            headA = headA.next;
+            headB = headB.next;
+        }
+
+        return headA;
+    }
+
+    public int getSize(ListNode node) {
+        ListNode temp = node;
+        int counter = 0;
+        while (temp != null) {
+            counter++;
+            temp = temp.next;
+        }
+        return counter;
+    }
+
 }
