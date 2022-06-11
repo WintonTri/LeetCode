95d167de47dfa1286779691eeb2e43f0e7cb73ff
winton9000 <86459736+WintonTri@users.noreply.github.com>
2022-06-11 01:30:59 -0700

    Update

---
+++ src/inprogress/Question557.java
@@ -0,0 +1,35 @@
+package inprogress;
+
+import java.util.ArrayList;
+
+// Attempt 1 Completed -> 06/11/2022
+public class Question557 {
+
+    public String reverseWords(String s) {
+        if (s == null)
+            return null;
+        ArrayList<String> words = new ArrayList<>();
+
+        for (String word : s.split(" ")) {
+            words.add(reverseString(word));
+        }
+
+        return String.join(" ", words);
+    }
+
+    public String reverseString(String s) {
+        char[] in = s.toCharArray();
+        int begin = 0;
+        int end = in.length - 1;
+        char temp;
+        while (end > begin) {
+            temp = in[begin];
+            in[begin] = in[end];
+            in[end] = temp;
+            end--;
+            begin++;
+        }
+        return new String(in);
+    }
+
+}
