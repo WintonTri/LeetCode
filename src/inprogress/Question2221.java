9558fc47789e9d12bde44a08a51c2c4671a3662a
winton9000 <86459736+WintonTri@users.noreply.github.com>
2022-06-11 01:15:27 -0700

    Update

---
+++ src/inprogress/Question2221.java
@@ -0,0 +1,21 @@
+package inprogress;
+
+// Attempt 1 Completed -> 06/11/2022
+public class Question2221 {
+
+    public int triangularSum(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return -1; // Return invalid answer
+
+        int iterations = 0;
+        while (iterations < nums.length) {
+            for (int i = 1; i < nums.length - iterations; i++) {
+                nums[i - 1] = (nums[i - 1] + nums[i]) % 10;
+            }
+            iterations++;
+        }
+
+        return nums[0];
+    }
+
+}
